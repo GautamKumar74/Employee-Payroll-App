77a8796cf91f3c71e385b069b407f7dfdab742c5
Gautam Kumar <gk62018@gmail.com>
2025-02-24 16:44:33 +0530

    [Gautam] Add . basic rest calls added and tested in uc1

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeeController.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.employeepayrollapp.controller;
+
+import com.bridgelabz.employeepayrollapp.model.Employee;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @GetMapping("/welcome")
+    public String welcomeMessage() {
+        return "Welcome to Employee Payroll App!";
+    }
+
+    @PostMapping("/add")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        return employee;  // Temporary response (No DB yet)
+    }
+}
