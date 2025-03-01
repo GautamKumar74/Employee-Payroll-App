6c819ba5aabf08882f02341a950fe23349cafff5
Gautam Kumar <gk62018@gmail.com>
2025-03-01 16:40:48 +0530

    [Gautam] Add . database setting as environment variable

--- src/main/java/com/bridgelabz/employeepayrollapp/model/Employee.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/model/Employee.java
@@ -1,56 +1,21 @@
 package com.bridgelabz.employeepayrollapp.model;
 import com.bridgelabz.employeepayrollapp.dto.EmployeeDTO;
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Employee {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
     private double salary;
-
-    // Constructors
-    public Employee() {}
-
-    // Constructor used for In-Memory Storage
-    public Employee(Long id, String name, double salary) {
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
-    }
-
-    // (Optional) Constructor using DTO
-    public Employee(Long id, EmployeeDTO dto) {
-        this.id = id; // ID comes from service layer
-        this.name = dto.getName();
-        this.salary = dto.getSalary();
-    }
-
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
 }
